12f8835d175ca1c45e1ae7882283a7b8f444b290
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-08 12:12:09 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,19 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import urllib, json
+import scraperwiki
+url = "https://data.police.uk/api/forces"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print data 
+print len(data)
+record = {}
+for i in data: 
+  print i['name']
+  if i['id'] == 'wiltshire':
+    print "This is Wiltshire"
+    #To look for text in a string: 
+    if 'wilt' in i['id']:
+      print "It contains 'wilt'"
+  record['idcode'] = i['id']
+  record['fullname'] = i['name']
+  scraperwiki.sqlite.save(['idcode'], record)
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
